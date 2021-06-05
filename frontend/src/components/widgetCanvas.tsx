ff0ea7c72afe82be416893d59d67ae5c42c12fe7
Varun Bankar <mail.varunbankar@gmail.com>
2021-06-05 22:25:10 +0530

    Add React Grid Layout

--- frontend/src/components/widgetCanvas.tsx
+++ frontend/src/components/widgetCanvas.tsx
@@ -4,14 +4,13 @@
 
 import React from "react";
 import { Box, makeStyles } from "@material-ui/core";
+import { Responsive, WidthProvider } from "react-grid-layout";
 
 
 const useStyles = makeStyles(
   (theme) => (
     {
-      root: {
-
-      }
+      root: {}
     }
   )
 );
@@ -21,15 +20,32 @@ interface WidgetCanvasProps {
 
 }
 
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const WidgetCanvas = ({}: WidgetCanvasProps) => {
 
   const classes = useStyles();
+  const layouts = {
+    lg: [
+    { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
+    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: "c", x: 4, y: 0, w: 1, h: 2 }
+  ]
+  };
 
   return (
     <>
       <Box className={classes.root}>
-
+        <ResponsiveGridLayout
+          className="layouts"
+          layouts={layouts}
+          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+        >
+          <div key="1">1</div>
+          <div key="2">2</div>
+          <div key="3">3</div>
+        </ResponsiveGridLayout>
       </Box>
     </>
   );
