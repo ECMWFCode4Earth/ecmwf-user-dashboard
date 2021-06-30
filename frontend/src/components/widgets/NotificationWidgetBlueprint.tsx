a76951766fb3efa7246a486b08511b319e7cd11a
Varun Bankar <mail.varunbankar@gmail.com>
2021-06-30 22:55:27 +0530

    Refactor NotificationWidgetBlueprint.tsx

---
+++ frontend/src/components/widgets/NotificationWidgetBlueprint.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Box, makeStyles } from "@material-ui/core";
+
+
+const useStyles = makeStyles(
+  (theme) => (
+    {}
+  )
+);
+
+
+interface NotificationWidgetBlueprintProps {
+
+}
+
+
+const NotificationWidgetBlueprint: React.FC<NotificationWidgetBlueprintProps> = ({}) => {
+
+  const classes = useStyles();
+
+  return (
+    <Box>
+
+    </Box>
+  );
+
+};
+
+
+export default NotificationWidgetBlueprint;
