11914debddfa71cf06ec54918aaf4f8c679a5b9d
asus <adarshnarayan0708@gmail.com>
2022-06-20 12:41:54 +0630

    Add TableWidgetBuilder file
    This file works as the layout file containing the visual and data configuration that a particular table widget will have

---
+++ client/src/models/widgetBuilders/TableWidgetBuilder.tsx
@@ -0,0 +1,24 @@
+// export {}
+import { Layout } from "react-grid-layout";
+
+import { WidgetBuilder } from "./WidgetBuilder";
+
+import SatelliteAlertsWidgetBlueprint from "../../components/widgets/SatelliteAlertsWidgetBlueprint";
+import TableWidgetBlueprint from "../../components/widgets/TableWidgetBlueprint";
+
+
+export class TableWidgetBuilder extends WidgetBuilder {
+
+    builderClassId = "table-widget"
+    layout: Layout = { i: this.widgetId, x: 0, y: Infinity, w: 3, h: 12, minW: 3, maxW: 4, minH: 7, maxH: 15 };
+
+    public build() {
+        console.log("trying the build")
+        return (
+            <div key={this.widgetId} data-grid={this.layout} /*style={{"backgroundColor":"black"}}*/>
+                <TableWidgetBlueprint builder={this} title={this.title} src={this.href} appURL={this.appURL}/>
+            </div>
+        );
+    }
+
+}
