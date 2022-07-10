6ea0462407d84cf8be5549134ce5cd52b1bee5cb
asus <adarshnarayan0708@gmail.com>
2022-07-11 05:07:49 +0630

    Add custom color support to Table Data
    This commit allows the data to render in different colors as per the api description of styling

--- client/src/components/widgets/TableWidgetBlueprint.tsx
+++ client/src/components/widgets/TableWidgetBlueprint.tsx
@@ -9,6 +9,7 @@ import {
 } from "@material-ui/core";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import axios from "axios";
+import _ from "lodash";
 
 import WidgetContainer from "../common/WidgetContainer";
 import WidgetTitleBar from "../common/WidgetTitleBar";
@@ -50,29 +51,15 @@ const TableDataWidgetBlueprint: React.FC<TableDataWidgetProps> = ({ builder, tit
     const [error, setError] = useState("");
     const [tableData, setTableData] = useState<any[]>([]);
     const [refresh, setRefresh] = useState<boolean>(false);
+    const [columnHead, setColumnHead] = useState<string[]>([]);
+    const [style, setStyle] = useState<Record<string, string>>({});
 
-
-/*    useEffect(() => {
-        fetchQuery().catch((err) => setError("An error occurred. Failed to fetch data from backend server."));
-    }, [refresh]);
-
-    const fetchQuery = async () => {
-        console.log(src)
-        const data = (await axios.get(`${src}`));
-        console.log("from TableWidgetBlueprint")
-        console.log(data)
-        if (data.status === 200) {
-            console.log(Object.keys(data.data.data[0]));
-            setTableData(data.data.data);
-        } else {
-            console.log("query error")
-            throw new Error("Backend query error.");
-        }
-        setLoading(false);
-    };*/
-
-    useEffect(() => {
-        const fetchData = async () => {
+    /*    useEffect(() => {
+            fetchQuery().catch((err) => setError("An error occurred. Failed to fetch data from backend server."));
+        }, [refresh]);`
+    
+        const fetchQuery = async () => {
+            console.log(src)
             const data = (await axios.get(`${src}`));
             console.log("from TableWidgetBlueprint")
             console.log(data)
@@ -84,8 +71,75 @@ const TableDataWidgetBlueprint: React.FC<TableDataWidgetProps> = ({ builder, tit
                 throw new Error("Backend query error.");
             }
             setLoading(false);
+        };*/
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = (await axios.get(`${src}`,
+                // {headers: {
+                //         "X-Auth-Token": process.env["X-AUTH-TOKEN"],
+                //         "content-type": "application/json"
+                //     }}
+            ));
+            //fixing the styling first....
+            // data->data->styling
+            //TODO: ask for the data like the previous format except the keywords
+            //getting the columns first
+            const cols = data.data.styling.columns;
+            console.log("cols: ", cols)
+
+            const colHeadTemp : string[] = [];
+            const styleTemp: Record<string, string> = {};
+            const tempObj = [];
+
+            //cols is an array
+            for(let i=0; i<cols.length; i++){
+                colHeadTemp.push(cols[i].title)
+                // let obj = _.cloneDeep(cols[i]);
+                tempObj.push(Object.entries(cols[i]));
+                // let obj = cols[i];
+            }
+            console.log("tempObj: ", tempObj)
+            // tempObj.forEach((obj:any) => {
+            //     styleTemp[obj.key] = obj?.color
+            // })
+            //TODO: see if I can make it more column specific
+            tempObj.forEach((somearr:any) => {
+                somearr.forEach((nested:any)=>{
+                    styleTemp[nested[0]] = nested[1].colour === undefined ? 'black' : nested[1].colour
+                })
+                // styleTemp[somearr[0]] = styleTemp[somearr[1]?.color]
+                // console.log("key: ", somearr[0], " val: ", styleTemp[somearr[0]])
+            })
+            console.log("styleTemp",styleTemp);
+
+
+
+            setColumnHead(colHeadTemp)
+            setStyle(styleTemp)
+            console.log("from TableWidgetBlueprint")
+
+            if (data.status === 200) {
+                console.log(Object.keys(data.data.data[0]));
+                console.log("Table data: ",data.data.data)
+                const dataToRender = data.data.data;
+                const obj:any[] = []
+                for(let i=0; i<dataToRender[0].length; i++){
+                    const tempObj = [];
+                    for(let j=0; j<dataToRender.length; j++){
+                        tempObj.push(dataToRender[j][i]);
+                    }
+                    obj.push(tempObj);
+                }
+                console.log("object:",obj)
+                setTableData(obj);
+            } else {
+                console.log("query error")
+                throw new Error("Backend query error.");
+            }
+            setLoading(false);
         }
-        fetchData().catch((err) => setError("An error occurred. Failed to fetch data from backend server."));
+        fetchData().catch((err) => setError(err));
     }, [refresh]);
 
 
@@ -97,7 +151,7 @@ const TableDataWidgetBlueprint: React.FC<TableDataWidgetProps> = ({ builder, tit
     if (error) return <WidgetError message={error} onClose={removeWidget}/>;
 
     if (loading) return <WidgetLoading onClose={removeWidget}/>;
-
+    console.log("columnHead: ", columnHead)
     return (
         <WidgetContainer>
             <WidgetTitleBar title={title} onClose={removeWidget}>
@@ -118,7 +172,7 @@ const TableDataWidgetBlueprint: React.FC<TableDataWidgetProps> = ({ builder, tit
                     <Table>
                         <TableHead>
                             <TableRow>
-                                {Object.keys(tableData[0]).map((key, index) => {
+                                {columnHead.map((key, index) => {
                                     return (
                                         <TableCell key={index}>
                                             {key.toUpperCase()}
@@ -132,11 +186,16 @@ const TableDataWidgetBlueprint: React.FC<TableDataWidgetProps> = ({ builder, tit
                             (data, index) => {
                                 return (
                                     <TableRow key={index}>
-                                        {Object.values(data).map((key:any) => {
-                                            return (<TableCell key={key}>
+                                        {data.map((key:any)=>{
+                                            return (<TableCell key={key} style={{color:style[key]}}>
                                                 {key}
                                             </TableCell>)
                                         })}
+                                        {/*{Object.values(data).map((key:any) => {
+                                            return (<TableCell key={key}>
+                                                {key}
+                                            </TableCell>)
+                                        })}*/}
                                     </TableRow>
                                 );
                             }
