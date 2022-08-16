79366406abc979888bd91299e8ef6008a5a5ed51
asus <adarshnarayan0708@gmail.com>
2022-08-16 21:45:03 +0530

    Modify AuthContext.tsx to include the functionality to get, post and delete api endpoints.

--- client/src/utils/contexts/AuthContext.tsx
+++ client/src/utils/contexts/AuthContext.tsx
@@ -1,13 +1,17 @@
-import React, { createContext, useEffect, useState } from "react";
-import { addDays } from "date-fns";
+import React, {createContext, useEffect, useState} from "react";
+import {addDays} from "date-fns";
 import axios from "axios";
 
-import { User } from "../../models/User";
+import {User} from "../../models/User";
 
 import localStore from "../localStore";
-import { kLocalStoreKey, kStore } from "../constants";
-import { UserDetails } from "../types";
+import {kLocalStoreKey, kStore} from "../constants";
+import {UserDetails} from "../types";
 
+interface Endpoint{
+  url: string,
+  token: string
+}
 
 const useAuth = () => {
   const [user, setUser] = useState<User | null>(null);
@@ -47,9 +51,10 @@ const useAuth = () => {
       username: res.data.data.username,
       token: res.data.data.token,
       tokenExpirationDate: addDays(Date.now(), Number(res.data.data.expiresInDays)),
+      widgetEndpoints: res.data.data.widgetEndpoints || {}
     };
     await localStore.setItem(kLocalStoreKey.USER_DETAILS, userDetails);
-    const user = new User(userDetails.name, userDetails.username, userDetails.token);
+    const user = new User(userDetails.name, userDetails.username, userDetails.token, userDetails.widgetEndpoints);
     setUser(user);
 
   };
@@ -68,7 +73,7 @@ const useAuth = () => {
       return;
     }
 
-    const user = new User(userDetails.name, userDetails.username, userDetails.token);
+    const user = new User(userDetails.name, userDetails.username, userDetails.token, userDetails.widgetEndpoints);
     setUser(user);
   };
 
@@ -120,8 +125,59 @@ const useAuth = () => {
       throw new Error(res.data.message);
     }
   };
+  //TODO: update this function to accomodate saving
+  interface Endpoint{
+    url: string;
+    token: string;
+  }
+  const addWidgetEndpoints = async (endpoints:Endpoint[]) => {
+    const body = {
+      endpoints: endpoints
+    }
+    const res = await axios.post(`${kStore.BASE_URL}/api/widget-endpoints`, body, {
+      headers: {
+        Authorization: user?.token
+      }
+    });
+    if (res.status !== 200) {
+      throw new Error(res.data.message);
+    }
+  }
 
+  const getWidgetEndpoints = async() => {
+    console.log("received the request to get endpoints")
+    const res = await axios.get(`${kStore.BASE_URL}/api/widget-endpoints`, {
+      headers: {
+        Authorization: user?.token
+      }
+    });
+    if (res.status !== 200) {
+      throw new Error(res.data.message);
+    }
+    console.log("Endpoints:", res.data.data.endpoints)
+    return res.data.data.endpoints
+  }
 
+  const deleteWidgetEndpoint = async(endpoint : {url: string, token:string}) => {
+    const res = await axios.post(`${kStore.BASE_URL}/api/widget-endpoints`, endpoint, {
+      headers: {
+        Authorization: user?.token
+      }
+    });
+    if (res.status !== 200) {
+      throw new Error(res.data.message);
+    }
+  }
+  const deleteAllWidgetEndpoints = async() => {
+    const res = await axios.post(`${kStore.BASE_URL}/api/delete-widget-endpoints`, {
+      headers: {
+        Authorization: user?.token
+      }
+    });
+    if (res.status !== 200) {
+      throw new Error(res.data.message);
+    }
+  }
   return {
     user,
     signup,
@@ -130,7 +186,11 @@ const useAuth = () => {
     isAuthenticated,
     changePassword,
     fetchApiTokens,
-    updateApiTokens
+    updateApiTokens,
+    addWidgetEndpoints,
+    getWidgetEndpoints,
+    deleteWidgetEndpoint,
+    deleteAllWidgetEndpoints
   };
 };
 
@@ -144,7 +204,11 @@ const AuthContext = createContext<ReturnType<typeof useAuth>>(
     isAuthenticated: () => false,
     changePassword: async () => {},
     fetchApiTokens: async () => {},
-    updateApiTokens: async () => {}
+    updateApiTokens: async () => {},
+    addWidgetEndpoints: async () => {},
+    getWidgetEndpoints: async () => [],
+    deleteAllWidgetEndpoints: async () => {},
+    deleteWidgetEndpoint: async () => {}
   }
 );
 
